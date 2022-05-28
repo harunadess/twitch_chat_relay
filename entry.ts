422d95d53b4b913848cc88122439149905cbd89a
harunadess <jortathlon@hotmail.com>
2022-05-28 16:01:30 +0100

    - update bot to do message forwarding

--- entry.ts
+++ entry.ts
@@ -1,8 +1,9 @@
 import * as tmi from 'tmi.js';
 import { logger } from './src';
-import { Identity } from './src/types'
+import { Identity, IGenericObjectOfType } from './src/types'
 
 const identity = require('../twitch_identity.json') as Identity;
+const commandToken = '!';
 
 // todo: oauth flow
 // todo: manage channels
@@ -11,17 +12,42 @@ const client = new tmi.Client({
   options: { debug: true },
   identity,
   channels: [
-    'harunadess'
+    'harunadess',
+    'otherchannel'
   ]
 });
+
+const channelForwardMapping = {
+  'harunadess': 'otherchannel',
+  'otherchannel': 'harunadess'
+} as IGenericObjectOfType<string>;
 
 client.on('chat', (channel: string, userState: tmi.ChatUserstate, message: string, self: boolean) => {
   if(self) return;
 
-  logger.info('From channel', channel, 'Message:', message);
-  logger.info('tags', userState);
-  client.say(channel, `@${userState.username} hello, world`);
+  switch(message.split(' ')[0]) {
+    case `${commandToken}forward`:
+      forwardMessage(channel, userState, formatMessage(`${commandToken}forward`, message));
+      break;
+  }
 });
+
+function formatMessage(toRemove: string | RegExp, msg: string): string {
+  return msg.replace(toRemove, '').trim();
+}
+
+function forwardMessage(channel: string, userState: tmi.ChatUserstate, message: string) {
+  const channelKey = channel.slice(1);
+  logger.info('userstate: ', userState);
+  const msg = `${userState.username} [in: ${channel}] said: ${message}`;
+
+  logger.info('channelKey:', channelKey);
+  const forwardingChannel = channelForwardMapping[channel.slice(1)];
+  if (forwardingChannel) {
+    logger.info('forwarding to ->', forwardingChannel);
+    client.say(`#${forwardingChannel}`, msg);
+  }
+}
 
 
 client.connect().then((value: [string, number]) => {
---
+++ src/types/generic.ts
@@ -0,0 +1,3 @@
+export interface IGenericObjectOfType<T> {
+  [p: string]: T;
+}
